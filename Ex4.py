a37a9fc9e193ba73659cec1543f2d8c05186d7a5
Tzach Cohen <TzachX@gmail.com>
2021-07-03 22:49:21 +0300

    Add files via upload

--- Ex4.py
+++ Ex4.py
@@ -1,6 +1,9 @@
 import torch
 import torch.nn as nn
+from torch.nn.utils.rnn import pack_padded_sequence, pad_packed_sequence
 import torch.nn.functional as F
+from torch import optim
+from torch.autograd import Variable
 import json
 import numpy as np
 from torch.utils.data import DataLoader
@@ -10,6 +13,120 @@ TEST_PATH = "data/snli_1.0_test.jsonl"
 DEV_PATH = "data/snli_1.0_dev.jsonl"
 GLOVE_PATH = "data/glove.840B.300d.txt"
 UNKNOWN = "UNKNOWN"
+DEVICE = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+EMBEDDING_DIM = 300
+
+
+
+class NLIModel(nn.Module):
+
+    def __init__(self, vecs, emb_d=EMBEDDING_DIM, mlp_d=1600, lstm_hidden_dims=[512, 1024, 2048], dropout=0.1):
+        super().__init__()
+        self.E = nn.Embedding.from_pretrained(vecs)
+        self.lstm = nn.LSTM(input_size=emb_d, hidden_size=lstm_hidden_dims[0],
+                            num_layers=1, bidirectional=True)
+
+        self.lstm_1 = nn.LSTM(input_size=(emb_d + lstm_hidden_dims[0] * 2), hidden_size=lstm_hidden_dims[1],
+                              num_layers=1, bidirectional=True)
+
+        self.lstm_2 = nn.LSTM(input_size=(emb_d + (lstm_hidden_dims[0] + lstm_hidden_dims[1]) * 2), hidden_size=lstm_hidden_dims[2],
+                              num_layers=1, bidirectional=True)
+
+        self.mlp_1 = nn.Linear(lstm_hidden_dims[2] * 2 * 4, mlp_d)
+        self.mlp_2 = nn.Linear(mlp_d, mlp_d)
+        self.sm = nn.Linear(mlp_d, 3)
+
+        self.classifier = nn.Sequential(*[self.mlp_1, nn.ReLU(), nn.Dropout(dropout),
+                                          self.mlp_2, nn.ReLU(), nn.Dropout(dropout),
+                                          self.sm])
+
+    def forward(self, x1, x1_l, x2, x2_l):
+        """
+        Args:
+            pre_input (Variable): A long variable containing indices for
+                premise words. Size: (max_length, batch_size).
+            pre_lengths (Tensor): A long tensor containing lengths for
+                sentences in the premise batch.
+            hyp_input (Variable): A long variable containing indices for
+                hypothesis words. Size: (max_length, batch_size).
+            pre_lengths (Tensor): A long tensor containing lengths for
+                sentences in the hypothesis batch.
+        Returns:
+            output (Variable): A float variable containing the
+                unnormalized probability for each class
+        :return:
+        """
+
+        x1_emb = self.E(x1)
+        x2_emb = self.E(x2)
+
+        #x1_packed = pack_padded_sequence(x1_emb, x1_l, batch_first=True, enforce_sorted=False)
+        #x2_packed = pack_padded_sequence(x2_emb, x2_l, batch_first=True, enforce_sorted=False)
+
+        packed_pinputs, r_index = pack_for_rnn_seq(x1_emb, x1_l)
+
+        output, (hn, cn) = self.lstm(packed_pinputs.float())
+
+        output_x1  = unpack_from_rnn_seq(output, r_index)
+
+        output_x1, (hn, cn) = self.lstm(x2_emb.float())
+        output_x2, (hn, cn) = self.lstm(x2_emb.float())
+
+        output_x1_unpacked, _ = unpack_from_rnn_seq(output_x1, batch_first=True)
+        output_x2_unpacked, _ = pad_packed_sequence(output_x2, batch_first=True)
+
+        # Length truncate
+        len1 = output_x1_unpacked.size(1)
+        len2 = output_x2_unpacked.size(1)
+        x1_emb = x1_emb[:len1, :, :]  # [T, B, D]
+        x2_emb = x2_emb[:len2, :, :]  # [T, B, D]
+
+        # Using residual connection
+        s1_layer2_in = torch.cat([x1_emb, output_x1_unpacked], dim=2)
+        s2_layer2_in = torch.cat([x2_emb, output_x2_unpacked], dim=2)
+
+        return x1_emb
+
+
+
+
+
+def pack_for_rnn_seq(inputs, lengths):
+    """
+    :param inputs: [T * B * D]
+    :param lengths:  [B]
+    :return:
+    """
+    _, sorted_indices = lengths.sort()
+    '''
+        Reverse to decreasing order
+    '''
+    r_index = reversed(list(sorted_indices))
+
+    s_inputs_list = []
+    lengths_list = []
+    reverse_indices = np.zeros(lengths.size(0), dtype=np.int64)
+
+    for j, i in enumerate(r_index):
+        s_inputs_list.append(inputs[:, i, :].unsqueeze(1))
+        lengths_list.append(lengths[i])
+        reverse_indices[i] = j
+
+    reverse_indices = list(reverse_indices)
+
+    s_inputs = torch.cat(s_inputs_list, 1)
+    packed_seq = pack_padded_sequence(s_inputs, lengths_list)
+
+    return packed_seq, reverse_indices
+
+
+def unpack_from_rnn_seq(packed_seq, reverse_indices):
+    unpacked_seq, _ = pad_packed_sequence(packed_seq)
+    s_inputs_list = []
+
+    for i in reverse_indices:
+        s_inputs_list.append(unpacked_seq[:, i, :].unsqueeze(1))
+    return torch.cat(s_inputs_list, 1)
 
 
 def parse_data(file):
@@ -32,25 +149,36 @@ def create_dict():
     print("Loading Glove Model")
     f = open(GLOVE_PATH, 'r', encoding="utf8")
     indexed_words = {}
+    words_to_vecs = {}
     vecs = []
     index = 0
     for line in f.readlines():
         splitLines = line.split(" ")
         indexed_words[splitLines[0]] = index
+        words_to_vecs[splitLines[0]] = np.asarray(splitLines[1:], dtype='float32')
         index += 1
-        vecs.append(np.array([float(value) for value in splitLines[1:]]))
-
+    f.close()
     indexed_words[UNKNOWN] = index
 
     print("Loading complete!")
-    return indexed_words, np.stack(vecs)
+    print("Creating Embedding matrix...")
+    embedding_matrix = np.random.random((index + 1, EMBEDDING_DIM))
+    for word in indexed_words:
+        embedding_vector = words_to_vecs.get(word)
+        if embedding_vector is not None:
+            embedding_matrix[indexed_words[word]] = embedding_vector
+    print("Creation complete!")
+    return indexed_words, torch.tensor(embedding_matrix)
 
 
 def create_dataset(premises, labels, hypotheses, indexed_words, indexed_labels):
     print("Creating dataset...")
     dataset = []
+    premises_tensors = []
+    labels_tensors = []
+    hypotheses_tensors = []
     for i in range(len(premises)):
-        premise, label, hypothesis = premises[i].replace('.', '').split(" "), labels[i], hypotheses[i].split(" ")
+        premise, label, hypothesis = premises[i].replace('.', '').replace('\n', '').split(" "), labels[i], hypotheses[i].replace('.', '').replace('\n', '').split(" ")
 
         for p in range(len(premise)):
             if premise[p] not in indexed_words:
@@ -60,98 +188,20 @@ def create_dataset(premises, labels, hypotheses, indexed_words, indexed_labels):
             if hypothesis[h] not in indexed_words:
                 hypothesis[h] = UNKNOWN
 
-        premise = torch.cat([torch.tensor([indexed_words[w]], dtype=torch.long) for w in premise])
-        hypothesis = torch.cat([torch.tensor([indexed_words[w]], dtype=torch.long) for w in hypothesis])
-        label = indexed_labels[label]
-        dataset.append((premise, hypothesis, label))
+        premises_tensors.append(torch.cat([torch.tensor([indexed_words[w]], dtype=torch.long) for w in premise]))
+        hypotheses_tensors.append(torch.cat([torch.tensor([indexed_words[w]], dtype=torch.long) for w in hypothesis]))
+        labels_tensors.append(torch.tensor(indexed_labels[label]))
+
+    premises_lens = [len(p) for p in premises_tensors]
+    hypotheses_lens = [len(h) for h in hypotheses_tensors]
+    premises_tensors = nn.utils.rnn.pad_sequence(premises_tensors).transpose(0, 1).tolist()
+    hypotheses_tensors = nn.utils.rnn.pad_sequence(hypotheses_tensors).transpose(0, 1).tolist()
+
+    for p, p_l, h, h_l, l in zip(premises_tensors, premises_lens, hypotheses_tensors, hypotheses_lens, labels_tensors):
+        dataset.append((p, p_l, h, h_l, l))
+
     print("Creation complete!")
     return dataset
-
-
-# TODO: change to the implementation of the authors
-class SentenceEncoder(nn.Module):
-    def __init__(self, pretrained_vecs, lstm_hidden_size, linear_hidden_size, output_size, lstm_layers):
-        super().__init__()
-
-        # Using pretrained vectors to initialize the embedding
-        self.embeddings = nn.Embedding.from_pretrained(pretrained_vecs)
-
-        # Creating a biLSTM with lstm_layers number of layers
-        self.lstm = nn.LSTM(input_size=embeddings_vec_size, hidden_size=lstm_hidden_size, bidirectional=True, num_layers=lstm_layers)
-        self.fc1 = nn.Linear(in_features=lstm_hidden_size, out_features=linear_hidden_size)
-        nn.init.xavier_uniform_(self.fc1.weight)
-        self.fc2 = nn.Linear(in_features=linear_hidden_size, out_features=output_size)
-        nn.init.xavier_uniform_(self.fc2.weight)
-
-        self.activation = nn.ReLU()
-
-    def forward(self, sequence):
-        embeds = self.embeddings(sequence)
-        lstm_input = embeds.view(len(sequence), 1, -1)
-        _, (hn, _) = self.lstm(lstm_input)
-        output = self.fc1(hn[0])
-        output = self.activation(output)
-        output = self.fc2(output)
-
-        return output
-
-
-class MLP(nn.Module):
-
-    def __init__(self, input_dim, hidden_dim, num_layers, dropout_prob):
-        super().__init__()
-        self.input_dim = input_dim
-        self.hidden_dim = hidden_dim
-        self.num_layers = num_layers
-        self.dropout_prob = dropout_prob
-
-        mlp_layers = []
-        for i in range(num_layers):
-            if i == 0:
-                layer_input_dim = input_dim
-            else:
-                layer_input_dim = hidden_dim
-            linear_layer = nn.Linear(in_features=layer_input_dim,
-                                     out_features=hidden_dim)
-            relu_layer = nn.ReLU()
-            dropout_layer = nn.Dropout(dropout_prob)
-            mlp_layer = nn.Sequential(linear_layer, relu_layer, dropout_layer)
-            mlp_layers.append(mlp_layer)
-        self.mlp = nn.Sequential(*mlp_layers)
-
-    def forward(self, input):
-        """
-        Args:
-            input (Variable): A float variable of size
-                (batch_size, input_dim).
-        Returns:
-            output (Variable): A float variable of size
-                (batch_size, hidden_dim), which is the result of
-                applying MLP to the input argument.
-        """
-
-        return self.mlp(input)
-
-
-class NLIClassifier(nn.Module):
-
-    def __init__(self, sentence_dim, hidden_dim, num_layers, num_classes,
-                 dropout_prob):
-        super().__init__()
-        self.sentence_dim = sentence_dim
-        self.hidden_dim = hidden_dim
-        self.dropout_prob = dropout_prob
-
-        self.mlp = MLP(input_dim=4 * sentence_dim, hidden_dim=hidden_dim,
-                       num_layers=num_layers, dropout_prob=dropout_prob)
-        self.clf_linear = nn.Linear(in_features=hidden_dim,
-                                    out_features=num_classes)
-
-    def forward(self, pre, hyp):
-        mlp_input = torch.cat([pre, hyp, (pre - hyp).abs(), pre * hyp], dim=1)
-        mlp_output = self.mlp(mlp_input)
-        output = self.clf_linear(mlp_output)
-        return output
 
 
 def main():
@@ -159,8 +209,38 @@ def main():
     indexed_labels = {"-": -1, "entailment": 0, "neutral": 1, "contradiction": 2}
     train_premises, train_labels, train_hypotheses = parse_data(TRAIN_PATH)
     train_dataset = create_dataset(train_premises, train_labels, train_hypotheses, indexed_words, indexed_labels)
-    train_dataloader = DataLoader(train_dataset, batch_size=64, shuffle=True)
-    print(train_dataset)
+    train_dataloader = DataLoader(train_dataset, batch_size=32, shuffle=True)
+    lr = 0.0002
+
+
+    model = NLIModel(vecs=vecs.to(DEVICE))
+
+    model.to(DEVICE)
+    optimizer = torch.optim.Adam(params=model.parameters(), lr=lr, weight_decay=0.25)  # Need to add decay according to article..
+    criterion = torch.nn.CrossEntropyLoss()
+
+    for epoch in range(3):  # loop over the dataset multiple times
+
+        for i, data in enumerate(train_dataloader, 0):
+            premise, premise_length, hypothesis, hypothesis_length, label = data
+            premise = torch.cat(premise).reshape(32, -1)
+            premise = premise.to(DEVICE)
+            hypothesis = torch.cat(hypothesis).reshape(32, -1)
+            hypothesis = hypothesis.to(DEVICE)
+            premise_length = premise_length.int()
+            hypothesis_length = hypothesis_length.int()
+            label = label.to(DEVICE)
+
+            # zero the parameter gradients
+            optimizer.zero_grad()
+            outputs = model(premise, premise_length, hypothesis, hypothesis_length)
+            label_pred = outputs.max(1)[1]
+            loss = criterion(outputs, label)
+            print("loss is " + str(loss))
+            accuracy = torch.eq(label, label_pred).float().mean()
+            print("acc is " + str(accuracy))
+            loss.backward()
+            optimizer.step()
 
 
 if __name__ == '__main__':
