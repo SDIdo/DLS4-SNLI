108e32cae92a6b16fb8e6cdc3f8cecf4ef72db9b
Ido <Bookidodo@gmail.com>
2021-07-06 23:03:34 +0300

    Update test_model.py

--- test_model.py
+++ test_model.py
@@ -1,22 +1,22 @@
-import argparse
-import logging
-import os
 from sys import argv
-import numpy as np
 import torch
-from torchtext import data, datasets
+from torch.nn.utils.rnn import pad_sequence
 import json
-from main import NLIModel
+from train_model import NLIModel
 from torch.utils.data import DataLoader
-import torch.nn as nn
+
+STUDENTS={'name1': 'Ido Natan',
+         'ID1': '305727802',
+         'name2': 'Tzach Cohen',
+         'ID2': '208930842'}
 
 DEVICE = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-NUM_WORDS = 4000001
-DIMENSIONS = 300
+NUM_WORDS = 400002
+DIMENSIONS = 50
 UNKNOWN = "UNKNOWN"
 
 class TestArgs():
-    def __init__(self, model_path, indexed_words_path, test_path='data/snli_1.0_test.jsonl'):
+    def __init__(self, model_path, test_path='data/snli_1.0_test.jsonl', indexed_words_path='indexed_words.json'):
         self.test_path = test_path
         self.model_path = model_path
         self.indexed_words_path = indexed_words_path
@@ -24,14 +24,27 @@ class TestArgs():
 def create_random_embedding(words, dimension):
     return torch.rand(words, dimension)
 
-def create_test_set(premises, hypotheses, indexed_words):
+def pad_collate(batch):
+  (x1, x2, l) = zip(*batch)
+  x1_lens = [len(p) for p in x1]
+  x2_lens = [len(h) for h in x2]
+
+  x1_pad = pad_sequence(x1, batch_first=True, padding_value=0)
+  x2_pad = pad_sequence(x2, batch_first=True, padding_value=0)
+
+  return x1_pad, x2_pad, x1_lens, x2_lens, l
+
+def create_test_set(premises, labels, hypotheses, indexed_words, indexed_labels):
     print("Creating dataset...")
     dataset = []
     premises_tensors = []
+    labels_tensors = []
     hypotheses_tensors = []
     for i in range(len(premises)):
-        premise, hypothesis = premises[i].replace('.', '').replace('\n', '').split(" "), hypotheses[
-            i].replace('.', '').replace('\n', '').split(" ")
+        premise, label, hypothesis = premises[i].replace('.', '').replace('\n', '').split(" "), labels[i], hypotheses[i].replace('.', '').replace('\n', '').split(" ")
+
+        if label == "-":
+            label = "entailment"
 
         for p in range(len(premise)):
             if premise[p] not in indexed_words:
@@ -41,62 +54,78 @@ def create_test_set(premises, hypotheses, indexed_words):
             if hypothesis[h] not in indexed_words:
                 hypothesis[h] = UNKNOWN
 
-        premises_tensors.append(torch.cat([torch.tensor([indexed_words[w]], dtype=torch.long) for w in premise]))
-        hypotheses_tensors.append(torch.cat([torch.tensor([indexed_words[w]], dtype=torch.long) for w in hypothesis]))
+        premises_tensors.append(torch.cat([torch.tensor([indexed_words.get(w)], dtype=torch.long) for w in premise]))
+        hypotheses_tensors.append(torch.cat([torch.tensor([indexed_words.get(w)], dtype=torch.long) for w in hypothesis]))
+        labels_tensors.append(torch.tensor(indexed_labels[label]))
 
-    premises_lens = [len(p) for p in premises_tensors]
-    hypotheses_lens = [len(h) for h in hypotheses_tensors]
-    premises_tensors = nn.utils.rnn.pad_sequence(premises_tensors).transpose(0, 1).tolist()
-    hypotheses_tensors = nn.utils.rnn.pad_sequence(hypotheses_tensors).transpose(0, 1).tolist()
-
-    for p, p_l, h, h_l, in zip(premises_tensors, premises_lens, hypotheses_tensors, hypotheses_lens):
-        dataset.append((p, p_l, h, h_l))
+    for p, h, l in zip(premises_tensors, hypotheses_tensors, labels_tensors):
+        dataset.append((p, h, l))
 
     print("Creation complete!")
     return dataset
 
 def parse_test_data(file):
     premises = []
+    labels = []
     hypotheses = []
 
-    print("Loading test data...")
+    print("Loading file data...")
     with open(file) as f:
         for line in f:
             parse_line = json.loads(line)
             premises.append(parse_line["sentence1"])
+            labels.append(parse_line["gold_label"])
             hypotheses.append(parse_line["sentence2"])
     print("Loading complete!")
-    return premises, hypotheses
+    return premises, labels, hypotheses
 
-def test(args):
+def batched_key_value(dict, keys):
+    values = []
+    for key in keys:
+        values.append(dict[key.item()])
+    return values
+
+def pred_routin(args):
     # lstm_hidden_dims = [int(d) for d in args.lstm_hidden_dims.split(',')]
     print('Building model...')
     vecs = create_random_embedding(NUM_WORDS, DIMENSIONS) # TODO ok to say 400k words?
     model = NLIModel(vecs=vecs) # TODO trying to save the cost of loading actual vecs from json.. hoping in the load_state
     model.load_state_dict(torch.load(args.model_path))
     model.eval()
-    model.cuda(DEVICE)
 
     print('Testing model...')
+    indexed_labels = {'-': -1, "entailment": 0, "neutral": 1, "contradiction": 2}
     indexed_labels_rev = {-1: 'not sure', 0: "entailment", 1: "neutral", 2: "contradiction"}
-    indexed_words = (open(args.indexed_words_path, "r")).read()
-    'indexed_words.json'
-    test_premises, test_hypotheses = parse_test_data(args.test_path)
-    test_dataset = create_test_set(test_premises, test_hypotheses, indexed_words)
-    test_dataloader = DataLoader(test_dataset, batch_size=32, shuffle=True)
+    indexed_words_file = open(args.indexed_words_path, 'r')
+    indexed_words = json.load(indexed_words_file)
+    test_premises, test_labels, test_hypotheses = parse_test_data(args.test_path)
+    test_dataset = create_test_set(test_premises, test_labels, test_hypotheses, indexed_words, indexed_labels)
+    test_dataloader = DataLoader(test_dataset, batch_size=32, shuffle=True, collate_fn=pad_collate)
     preds = []
+    num_correct = 0
+    num_data = len(test_dataset)
     for batch in test_dataloader:
-        pre_input, pre_lengths = batch[0], batch[1]
-        hyp_input, hyp_lengths = batch[2], batch[3]
-        model_output = model(pre_input, pre_lengths, hyp_input, hyp_lengths)
+        premise, hypothesis, premise_length, hypothesis_length, label = batch
+        premise = premise.to(DEVICE)
+        hypothesis = hypothesis.to(DEVICE)
+        label = torch.tensor(label).to(DEVICE)
+        model_output = model(premise, premise_length, hypothesis, hypothesis_length)
         label_pred = model_output.max(1)[1]
-        preds.append([pre_input, hyp_input, indexed_labels_rev[label_pred]])
-
+        preds.append(batched_key_value(indexed_labels_rev, label_pred)) # TODO if time add the actual prem and hypo considering the batch..
+        num_correct_batch = torch.eq(label, label_pred).long().sum()
+        num_correct_batch = num_correct_batch.item()
+        num_correct += num_correct_batch
     with open('test.pred', "w") as f:
+        f.write('Assess model on the snli testset\n')
+        f.write('Students: Ido Nathan 30572782 | Tzach Cohen 208930842\n')
+        f.write('************************************\n')
         for pred in preds:
-            f.write(str(pred))
+            for label in pred:
+                f.write(str(label))
+                f.write("\n")
+        f.write("\n")
+        f.write(f'Model Accuracy: {round(num_correct / num_data,3)}')
 
 if __name__ == "__main__":
     args = TestArgs(*argv[1:])
-    test(args)
-
+    pred_routin(args)
